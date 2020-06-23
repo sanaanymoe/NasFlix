f4096bcfb7617bf058dde2c48d34c690b27c0675
sanaanymoe <sanaany1992@gmail.com>
2020-06-22 23:20:32 -0700

    finished redux cycle for users and session to creating the store

--- frontend/nas_flix.jsx
+++ frontend/nas_flix.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import * as SessionAPIUtil from './util/session_api_util' 
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-
+    const store = configureStore();
     ReactDOM.render(<h1>Welcome To NasFlix!!</h1>, root)
 
     //test stuff 
     window.login = SessionAPIUtil.login
     window.logout = SessionAPIUtil.logout
     window.signup = SessionAPIUtil.signup
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
 });
