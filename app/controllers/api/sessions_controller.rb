fecc724dea40a7ca7f9f41962458508ab69c8d92
sanaanymoe <sanaany1992@gmail.com>
2020-06-22 18:22:10 -0700

    added backend Auth, session util, session actions and session reducer

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,23 @@
+class Api::SessionsController < ApplicationController
+
+    def create 
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+
+        if @user 
+            login(@user)
+            render "api/users/show"
+        else 
+            render json: ["Invalid email/password combination"], status: 401
+        end 
+    end 
+
+    def destroy
+        @user = current_user 
+        if @user 
+            logout
+            render json: {}, status: 200  #for now may change it later 
+        else 
+            render json: ["You are not signed in!"], status: 404
+        end 
+    end 
+end
--- frontend/nas_flix.jsx
+++ frontend/nas_flix.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as SessionAPIUtil from './util/session_api_util' 
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
 
     ReactDOM.render(<h1>Welcome To NasFlix!!</h1>, root)
+
+    //test stuff 
+    window.login = SessionAPIUtil.login
+    window.logout = SessionAPIUtil.logout
+    window.signup = SessionAPIUtil.signup
 });
